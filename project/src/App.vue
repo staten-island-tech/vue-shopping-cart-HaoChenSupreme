f340cd3a32fcf6f95abf38f59f4b1138255a6984
HaoChenSupreme <HAoranc3@nycstudents.net>
2022-01-27 13:58:47 -0500

    rgrrge

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <h1>Hey</h1>
+    <h1 class="header">Novels for Sale</h1>
     <div class="display">
       <div class="card" v-for="novel in novels" :key="novel.name">
       <img class="card-img" :src="novel.img" :alt="novel.name">
@@ -84,31 +84,76 @@ export default {
 </script>
 
 <style>
-#app {
+#app, html{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  padding: 0rem;
+  margin: 0rem;
+  box-sizing: border-box;
+  font-size: 62.5%;
   color: #2c3e50;
+  background-color: rgb(250, 223, 223);
 }
 
 .display{
-  display: flex;
-  flex-direction: column;
-  padding: 0rem;
-  margin: 0rem;
+ display: flex;
+ flex-direction: row;
+ justify-content: space-around;
+ flex-wrap: wrap;
+ margin-left: 5rem;
+ margin-right: 5rem;
+}
+:root{
+  --h1: 9.594rem;
+  --h2: 3.063rem;
+  --h3: 2.375rem;
+  --h4: 1.25rem;
+}
+
+.header{
+  margin: 1rem;
+}
+
+.card-price, .card-body{
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  font-size: var(--h1);
+}
+
+h2 {
+  font-size: var(--h2);
+}
+
+h3 {
+  font-size: var(--h3);
+}
+
+h4 {
+  font-size: var(--h4);
 }
 
 .card{
-  display: flex;
-  flex-direction: column;
-  padding: 0rem;
-  margin: 0rem;
+  width: 35vh;
+  height: 50vh;
+  border: 0.5rem solid;
+  border-color: rgb(243, 122, 122);
+  background-color: rgb(165, 216, 250);
+  border-radius: 1rem;
+  padding-top: 3rem;
+  margin: 1rem;
 }
 
 .card-img{
-  width: 30vh;
-  height: 40vh;
+  width: 26vh;
+  height: 39vh;
+  border: 0.5rem solid;
+  border-color: rgb(204, 165, 248);
+  border-radius: 1rem;
 }
 
 </style>
