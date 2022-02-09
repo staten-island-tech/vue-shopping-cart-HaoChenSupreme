f2e4bc1441217feea6eb9d43c3939eb8b3a83e3a
HaoChenSupreme <HAoranc3@nycstudents.net>
2022-02-09 13:53:22 -0500

    dsrrgr

--- project/src/main.js
+++ project/src/main.js
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import router from "./router/index.js";
+import store from "./store/index";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  store: store,
+
   render: function (h) {
     return h(App);
   },
